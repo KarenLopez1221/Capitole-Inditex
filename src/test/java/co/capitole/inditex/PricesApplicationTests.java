bb3dd58edc6ab2783bc0753543fbf351720a703c
Karen Lopez <lopezka.12@gmail.com>
2023-02-11 15:47:48 -0500

    Cargue inicial proyecto

---
+++ src/test/java/co/capitole/inditex/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.capitole.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
